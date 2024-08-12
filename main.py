e8b022bfdeb263ae5b84c9fb1d1a36f46f34e057
itsmy-bday <itsmybirthdayim7@gmail.com>
2024-08-12 18:29:17 +0000

    added login + more game logic

--- main.py
+++ main.py
@@ -20,6 +20,89 @@ import random
 keys = db.keys()
 values = db.values()
 
+def refreshvariables():
+  findvariables = db[username + "1"]
+  findvariablesforstonk = db.get(username + "2")
+  findvariable_history = db.get(username + "3")
+
+  moneyexpereience = findvariables.split()
+  moneyexpereiences = findvariablesforstonk
+  global money
+  global experience_level
+  global stocks
+  global history
+  money = moneyexpereience[0]
+  experience_level = moneyexpereience[1]
+  stocks = moneyexpereiences
+  history = findvariable_history
+
+def menue():
+  rounded_moocha = round(float(money), 2)
+  formattedmoocha = "{:,}".format(rounded_moocha)
+
+  refreshvariables() 
+  time.sleep(0.001)
+  refreshvariables() 
+
+  print("\nMoney: ", formattedmoocha, "$")
+  print("Experience level: ", experience_level)
+  print("\nStocks:\n")
+  for k, v in stocks.items():
+    v_as_notzero = ""
+    if v == 0:
+      v_as_notzero = "-"
+    elif v != 0:
+      v_as_notzero = v
+    print(f"{k}: {v_as_notzero}")
+
+  answr = input("\nWrite 'stocks', 'codes', 'history' or 'mystocks' to go there: ")
+
+
+def gamestart():
+  menue()
+
+def loading_screen(num):
+  countxxx = 0
+  for i in range(1, num * 3 + 1):
+      if countxxx == 0:
+          print("Loading.")
+      elif countxxx == 1:
+          print("Loading..")
+      elif countxxx == 2:
+          print("Loading...")
+          countxxx = -1 # reset to -1 since count will be incremented below
+      countxxx += 1
+      time.sleep(0.01) # adds a delay 0.8
+
+def finduser(): #Gets the account info 
+  global username1
+  global username2
+  global username3
+  global money
+  global experience_level
+  global stocks
+  global history
+
+  username1 = username + "1"
+  username2 = username + "2"
+  username3 = username + "3"
+
+  findvariables = db[username + "1"]
+  findvariablesforstonk = db.get(username + "2")
+  findvariable_history = db.get(username + "3")
+
+
+  moneyexpereience = findvariables.split()
+
+  moneyexpereiences = findvariablesforstonk #keep
+
+  history = findvariable_history
+  money = moneyexpereience[0]
+  experience_level = moneyexpereience[1]
+  stocks = moneyexpereiences
+
+  loading_screen(random.choice([0, 1, 2]))
+  gamestart()
 
 def signup():
   usrnme = input("\nEnter your desired username: ") 
@@ -57,6 +140,22 @@ def signup():
       db[usrnme + "2"] = {'DOGE-CAD': 0, 'SBUX': 0, 'AAPL': 0, 'TSLA': 0, 'AMZN': 0, 'MSFT': 0, 'NVDA': 0, 'META': 0, 'NFLX': 0, "ISRG": 0, 'BA': 0, 'ADBE': 0, 'MA': 0, 'RML.NS': 0, 'ZC=F': 0, 'EQIX': 0, 'REGN': 0, 'MELI': 0, 'MKL': 0, 'MTD': 0, 'CMG': 0, '^RUT': 0, 'TPL': 0, "ALI=F": 0, 'AZO': 0, 'BKNG': 0, 'SEB': 0, 'NVR': 0, 'NXT.L': 0, 'LDSVF': 0, 'LISP.SW': 0, 'BTC-CAD': 0, 'LISN.SW': 0, '0QKN.L': 0, 'BRK-A': 0} # the stocks. 
       db[usrnme + "3"] = ["You made an account!"] #history
       Start() #exits so they need to use their username to sign in
+
+def login():
+  global username
+  username = input("\nEnter your username?: ")
+  if username not in keys:
+    print("Invalid username. Please try again. (run again to go to menu)")
+    login()
+  else:
+      password = getpass.getpass(prompt = "\nEnter your password: ", stream = None)
+      print('*' * len(password))
+      print("\n")
+      if password != db[username]:
+        print("That password does not match the username. Try again.")
+        login()
+      else:
+        finduser()
 
 def Start():
   while True:
